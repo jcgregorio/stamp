1cd6587b7d73c16d35f9d80a16a1e6fb1d3d1e29
Joe Gregorio <jcgregorio@google.com>
2015-08-16 22:45:24 -0400

    Add comments, also speed up code

--- src/templating.js
+++ src/templating.js
@@ -9,11 +9,15 @@ var Stamp = Stamp || {};
     this.doc = (document.currentScript||document._currentScript).ownerDocument;
   };
 
+  // Import the element 'id' from the HTML Import into the current docuemnt.
   ns.Context.prototype.import = function(id) {
     return document.importNode(this.doc.querySelector('#'+id).content, true);
   };
 
   // Utiltity function for creating a custom element.
+  //
+  // name - The name of the custom element.
+  // proto - And object to use as the elements prototype.
   ns.newElement = function(name, proto) {
     var ep = Object.create(HTMLElement.prototype);
     Object.keys(proto).forEach(function(key) {
@@ -28,12 +32,13 @@ var Stamp = Stamp || {};
   }
 
   // Regex to grab double moustache'd content.
-  var re = /{{\s([\w\.\^]+)\s}}/;
+  var re = /{{\s([\w\.\^]+)\s}}/g;
 
-  // Returns 'state' after applying address, where address is an array of string.
+  // Returns 'state' after applying address, where address is an array of
+  // string.
   //
-  // I.e. if the state is {a: {b: [9, 8, 7]}} then the address
-  // of ["a", "b", 0] would return 9.
+  // I.e. if the state is {a: {b: [9, 8, 7]}} then the address of ["a", "b",
+  // 0] would return 9.
   function filterState(address, state) {
     var mystate = state;
     for (var i = 0, len = address.length; i < len; i++) {
@@ -52,8 +57,8 @@ var Stamp = Stamp || {};
     return str.slice(0, index) + add + str.slice(index + count);
   }
 
-  // Extracts an address path, i.e. ["a", "b", "0"] from the input string
-  // 's', such as from "{{a.b.0}}".
+  // Extracts an address path, i.e. ["a", "b", "0"] from the input string 's',
+  // such as from "{{a.b.0}}".
   function addressOf(s) {
     if ((match = re.exec(s)) != null) {
       return match[1].split(".");
@@ -62,26 +67,32 @@ var Stamp = Stamp || {};
     }
   }
 
-  // Keeps expanding double moustache in the text content until there no
-  // more double moustache instances. Returns null if no templates were
-  // found, otherwise returns the expanded string.
+  // Keeps expanding double moustache in the text content until there no more
+  // double moustache instances. Returns null if no templates were found,
+  // otherwise returns the expanded string.
   function expandString(s, state) {
     var match;
     var found = false;
+    var matches = [];
+
     while ((match = re.exec(s)) != null) {
-      found = true;
-      address = match[1].split(".");
-      m = filterState(address, state);
+      matches.push(match);
+    }
+
+    for (var i = matches.length - 1; i >= 0; i--) {
+      match = matches[i];
+      var address = match[1].split(".");
+      var m = filterState(address, state);
       s = ssplice(s, match.index, match[0].length, m);
     }
-    if (found) {
+    if (matches.length) {
       return s;
     }
     return null;
   }
 
-  // Takes an array of nodes and returns an array of cloned nodes
-  // in the same order.
+  // Takes an array of nodes and returns an array of cloned nodes in the same
+  // order.
   function cloneAllNodes(a) {
     var clones = [];
     for (var i = 0, len = a.length; i < len; i++) {
@@ -97,19 +108,24 @@ var Stamp = Stamp || {};
     }
   }
 
+  // Removes all the children of 'ele'.
   function removeChildren(ele) {
-    while (ele.firstChild) {
-      ele.removeChild(ele.firstChild);
-    }
+    ele.innerHTML = "";
   }
 
+  // Removes all the children of 'ele' and then adds 'nodes' as ele's
+  // children.
   function replaceChildren(ele, nodes) {
-    removeChildren(ele);
+    ele.innerHTML = "";
     appendChildren(ele, nodes);
   }
 
   // Expand all the double moustaches found in the node
   // 'ele' and all its children against the data in 'state'.
+  //
+  // When descending into nested templates, i.e. a data-repeat- template then
+  // '^' is added to the child state as a way to access the data in the
+  // parent's scope.
   function expand(ele, state) {
     if (!Array.isArray(ele)) {
       ele = [ele];
@@ -194,11 +210,15 @@ var Stamp = Stamp || {};
     return ele;
   };
 
+  // Expand the template 'ele' with 'state' and then replace all the children
+  // of 'target' with the expanded content.
   function expandInto(target, ele, state) {
     replaceChildren(target, expand(ele, state));
   }
 
-  // <host-content> acts similar to the classic <content select="[css selector]"></content>
+  // <host-content> acts similar to the classic <content select="[css selector]"></content>,
+  // but as of today <content> isn't standardized and may change in the
+  // future, so we use our own custom element for now.
   ns.newElement('host-content', {});
 
   // Look for <host-content> elements in target and use their select's to pick
